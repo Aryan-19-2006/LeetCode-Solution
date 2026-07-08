d067f3308050a266208ec598dbb4563126a8243a
Aryan-19-2006 <aryangupta20061@gmail.com>
2026-07-08 10:33:31 +0530

    Time: 0 ms (100%), Space: 8.9 MB (68.26%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int n=s.size(),c=0;
+        int i =n-1;
+        while((i>=0) && s[i] == ' '){
+            i--;}
+        while(i>-1 && (s[i]!= ' ')){
+            i--;
+            c++;
+        }return c;
+    }
+};
