62bcc080f493a2ce131016f4278a1424583adf44
Aryan-19-2006 <aryangupta20061@gmail.com>
2026-08-05 20:26:12 +0530

    Time: 16 ms (100.00%) | Memory: 254.4 MB (99.66%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -1,43 +1,50 @@
+constexpr int N=1e5, M=2e5;
+struct Edge{ int v, nxt=-1; };
+Edge E[M];
+int eIdx=0;
+int adj[N];// heads of linked lists
+inline void addEdge(int u, int v){
+    E[eIdx]={v, adj[u]};
+    adj[u]=eIdx++;
+}
+uint8_t vis[N];
 class Solution {
 public:
-    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
-        vector<vector<int>> adj(n);
-
-        for (auto e : invocations) {
-            adj[e[0]].push_back(e[1]);
+    static void dfs(int u){
+        vis[u]=1;
+        for(int idx=adj[u]; idx!=-1; idx=E[idx].nxt){
+            const int v=E[idx].v;
+            if (vis[v]!=0) continue;
+            dfs(v);
         }
-
-        // Find all suspicious methods
-        vector<bool> suspicious(n, false);
-
-        function<void(int)> dfs = [&](int u) {
-            suspicious[u] = true;
-            for (int v : adj[u]) {
-                if (!suspicious[v])
-                    dfs(v);
-            }
-        };
-
+    }
+    static vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        eIdx=0;
+        memset(adj, -1, n*sizeof(int));
+        memset(vis, 0, n);
+        for(auto& e : invocations){
+            const int u=e[0], v=e[1];
+            addEdge(u, v);
+        }
         dfs(k);
-
-        // Check if any non-suspicious method calls a suspicious one
-        for (auto &e : invocations) {
-            int u = e[0], v = e[1];
-            if (!suspicious[u] && suspicious[v]) {
-                vector<int> ans;
-                for (int i = 0; i < n; i++)
-                    ans.push_back(i);
-                return ans;
+        bool cnnt1=0;
+        for(auto& e : invocations){
+            const int u=e[0], v=e[1];
+            if ((vis[u]==0) & (vis[v]==1)){
+                cnnt1=1;
+                break;
             }
         }
-
-        // Return all non-suspicious methods
         vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            if (!suspicious[i])
-                ans.push_back(i);
+        if (cnnt1){
+            ans.resize(n);
+            iota(ans.begin(), ans.end(), 0);
+            return ans;
         }
-
+        ans.reserve(n);
+        for(int i=0; i<n; i++){
+            if (vis[i]==0) ans.push_back(i);
+        }
         return ans;
     }
 };
